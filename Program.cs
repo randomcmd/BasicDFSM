964cc56bbd0d0fc814bf3632eb7d974be32b1728
randomcmd <63674603+randomcmd@users.noreply.github.com>
2020-09-06 21:14:55 +0200

    Added new spam filter example

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ namespace Finite_Deteministic_State_Machine
                 string input = Console.ReadLine();
                 if (input.Equals("0")) test0();
                 if (input.Equals("1")) test1();
+                if (input.Equals("2")) test2();
                 if (input.Equals("exit")) break;
             }
         }
@@ -89,6 +90,84 @@ namespace Finite_Deteministic_State_Machine
                 string input = Console.ReadLine();
                 if(input.Equals("exit")) break;
                 Console.WriteLine("Coffe machine still clean : " + fdsm.ValidateInput(input));
+            }
+        }
+
+        public static void test2()
+        {
+            //Example: Creating a state machine that checks if a word contains oop
+
+            //We use a string with all letters to loop through the "everything but this letter" transitions
+            string abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvw";
+            //Example: I can disclude a character with abc.Replace('a',String.Empty)
+
+
+            FDSM fdsm = new FDSM();
+            fdsm.SetStart("q0", StateType.DEFAULT);
+            fdsm.NewState("o0", StateType.DEFAULT);
+            fdsm.NewState("o1", StateType.DEFAULT);
+            fdsm.NewState("p", StateType.ACCEPTED);
+
+            //OOP transitions
+            fdsm.AddTransition("q0","o0",'o');
+            fdsm.AddTransition("o0","o1",'o');
+            fdsm.AddTransition("o1","p",'p');
+            //Also for the capital letters
+            fdsm.AddTransition("q0","o0",'O');
+            fdsm.AddTransition("o0","o1",'O');
+            fdsm.AddTransition("o1","p",'P');
+
+            //Now the anything but the letter something transitions
+            //We save a lot of work here by using foreach to cycle through the abc string insdead of adding each manually
+
+            //q0 to q0 with any letter but o
+            string abcModified = abc.Replace("o",String.Empty);
+            abcModified = abcModified.Replace("O",String.Empty);
+
+            //Console.WriteLine(abcModified);
+            //test0();
+            foreach(char c in abcModified)
+            {
+                fdsm.AddTransition("q0","q0",c);
+            }
+
+            //o0 to q0 with anything but o 
+            abcModified = abc.Replace("o",String.Empty);
+            abcModified = abcModified.Replace("O",String.Empty);
+
+            foreach(char c in abcModified)
+            {
+                fdsm.AddTransition("o0","q0",c);
+            }
+
+            //o1 to q0 with anything but p , but o is a special case cause double o
+            abcModified = abc.Replace("p",String.Empty);
+            abcModified = abcModified.Replace("P",String.Empty);
+            abcModified = abcModified.Replace("O",String.Empty);
+            abcModified = abcModified.Replace("o",String.Empty);
+
+            foreach(char c in abcModified)
+            {
+                fdsm.AddTransition("o1","q0",c);
+            }
+
+            fdsm.AddTransition("o1","o1", 'o');
+            fdsm.AddTransition("o1","o1", 'O');
+
+            //p to p with anything
+            foreach(char c in abc)
+            {
+                fdsm.AddTransition("p","p",c);
+            }
+
+            //Starting test 
+            Console.WriteLine("This FDSM is meant to replicate a spam filter which filters the word 'oop': ");
+            for(;;)
+            {
+                Console.Write("Enter a keyword: ");
+                string input = Console.ReadLine();
+                if(input.Equals("exit")) break;
+                Console.WriteLine("The word contains oop and is filtered : " + fdsm.ValidateInput(input));
             }
         }
     }
